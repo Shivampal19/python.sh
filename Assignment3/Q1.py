8238cd94c0be8faf099a7be7371334e825ed9ba7
Shivampal19 <Pal04558@gmail.com>
2024-06-09 17:29:33 +0530

    Add files via upload

---
+++ Assignment3/Q1.py
@@ -0,0 +1,3 @@
+#1. Write a python script to convert a number into str type.
+x = 52
+print(type(str(x)))
---
+++ Assignment3/Q5.py
@@ -0,0 +1,3 @@
+#5. Write a python script to print any number and its octal equivalent.
+x=56
+print(oct(x))
