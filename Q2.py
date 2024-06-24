96cc93c6dff2f5ed7a6dd76ab5c6df7f28614050
Shivampal19 <Pal04558@gmail.com>
2024-06-24 18:51:03 +0530

    Delete Q2.py

--- Q2.py
+++
@@ -1,9 +0,0 @@
-#2. Write a python script to print first N natural numbers
-
-Nnumber = int(input("Enter N Number "))
-FirstNum = 1
-
-while(FirstNum <= Nnumber):
-    print(FirstNum)
-
-    FirstNum +=1
