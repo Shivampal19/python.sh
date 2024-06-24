c12909b9d7c95b47276a4dd181f7716c240e9f46
Shivampal19 <Pal04558@gmail.com>
2024-06-24 18:51:55 +0530

    Delete Q8.py

--- Q8.py
+++
@@ -1,9 +0,0 @@
-#8. Write a python script to print squares of first N natural numbers
-
-Nnumber = int(input("Enter any Number "))
-NaturalNum = 1
-
-while(NaturalNum <= Nnumber):
-    print(NaturalNum**2)
-
-    NaturalNum +=1
