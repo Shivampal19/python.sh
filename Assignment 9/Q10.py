20bcf17e597959ca3c1257db004fccf372d4ae3b
Shivampal19 <Pal04558@gmail.com>
2024-06-26 21:23:12 +0530

    Add files via upload

---
+++ Assignment 9/Q10.py
@@ -0,0 +1,9 @@
+#10. Write a python script to print first 10 multiples of N
+
+Nnumber = int(input("Enter any Number "))
+NaturalNum = 1
+
+while(NaturalNum <= Nnumber):
+    print(NaturalNum**10)
+
+    NaturalNum +=1
