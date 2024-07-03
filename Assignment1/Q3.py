18c48ce1744f034e9122e6028bc7a273fc25957b
Shivampal19 <Pal04558@gmail.com>
2024-07-03 22:20:18 +0530

    Update Q3.py

--- Assignment1/Q3.py
+++ Assignment1/Q3.py
@@ -1,1 +1,6 @@
-#3. Write a python script and store it in a file and execute the file from the command line.
+#3. Write a python script and store it in a file and execute the file from the command line. 
+
+print('Learing Python')
+
+# Command to run python files in Command line / Terminal 
+# Python ./Assignment_01/Question_03.py
