690bb5f65b85d4cd384f5c0152d338fdf964cd1b
Shivampal19 <Pal04558@gmail.com>
2024-07-03 22:57:25 +0530

    Update Q3.py

--- Assignment2/Q3.py
+++ Assignment2/Q3.py
@@ -1,11 +1,15 @@
-#3. Write a python script to print types of variables. Create 5 variables each of them containing different types of data. (like 35, True, “MySirG”,5.46, 3+4j, etc)
-a=2
-print(type(a))
-b=3.5
-print(type(b))
-c="why"
-print(type(c))
-d=3+4j
-print(type(d))
-e=True
-print(type(e))
+# Write a python script to print type of variable. Create 5 variable each of them containing different type of data like 35, True, "MySirG", 5.46, 3+4j
+
+var1 = 35
+var2 = True
+var3 = "MySirG"
+var4 = 5.46
+var5 = 3 + 4j
+
+# print with its Data types
+
+print("var1 = ", var1, "and its Data type is ", type(var1))
+print("var2 = ", var2, "and its Data type is ", type(var2))
+print("var3 = ", var3, "and its Data type is ", type(var3))
+print("var4 = ", var4, "and its Data type is ", type(var4))
+print("var5 = ", var5, "and its Data type is ", type(var5))
