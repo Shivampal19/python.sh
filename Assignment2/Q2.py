c73a10df53f00bd8242c70a487381d2d0207f62b
Shivampal19 <Pal04558@gmail.com>
2024-07-03 22:54:57 +0530

    Update Q2.py

--- Assignment2/Q2.py
+++ Assignment2/Q2.py
@@ -1,4 +1,9 @@
 #2. Write a python script to add multi line comments and print values of four variables,each in a new line. Variable contains any values.
+
+"""
+multiline coments
+"""
+# variable decleration
 a=2
 b=3
 c=4
